db15debe5d297c9a39fd9dfe15867b42a2c031fb
space <enaenen@naver.com>
2023-05-03 13:20:23 +0900

    input examples

--- java/src/inf/Main.java
+++ java/src/inf/Main.java
@@ -1,7 +1,22 @@
 package inf;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        //Buffered Reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+
+        //Scanner
+        Scanner sc = new Scanner(System.in);
+        String next = sc.next();
+        int i = sc.nextInt();
+        char[] charArray = next.toCharArray();
         System.out.println("Hello world!");
     }
 }
